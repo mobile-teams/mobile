189029a1451b194a226d4508fa2e6027a1cdb2f2
shenlongfei1217 <328238432@qq.com>
2019-01-27 16:51:13 +0800

    新加load自定义方法

--- pages/index/index.js
+++ pages/index/index.js
@@ -93,4 +93,39 @@ Page({
       }
     });
   },
+  load: (that)=>{    
+    my.httpRequest({
+      url: 'http://192.168.54.77:8089/app-web/personal/public/gjjdkjbxxcx.service',
+      method: 'POST',
+      headers: {
+            "Content-Type": "application/json",
+            "citycode":"C23020KF"
+          },
+      data: {
+        appid: "20170517000101",
+        citybm:"C23020KF",
+        sign:"SYWDJSKI8UYH7D7FKIUJNE45IJHYRKJ0",
+        xingming:"乔铁军",
+       	zjhm:"230202196701261830"
+      },
+      dataType: 'json',
+      contentType : 'application/json;charset=UTF-8', //contentType很重要    
+      success: (res) => {
+        console.log(res);
+        grzhye1 = res.data.data[0].gjjxx[0].grzhye       
+        this.setData({ 
+          grzhye: grzhye1,
+          grzh:res.data.data[0].gjjxx[0].grzh,
+          grzhzt:res.data.data[0].gjjxx[0].grzhzt
+        }) 
+        app.setGrzh(res.data.data[0].gjjxx[0].grzh)
+        app.setJkhtbh(res.data.data[0].dkxx[0].jkhtbh)
+      }
+    });
+  },
+  onPullDownRefresh(){
+      this.abc1(this);
+      my.stopPullDownRefresh();
+    },
+    
 })
