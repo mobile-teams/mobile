7724a56dcc2d96e6a8122bce106a05383a381323
CBreakSoft <du5248jiaogufu2@163.com>
2019-02-15 16:18:15 +0800

    离退休提取---增加银行卡、提交审批

--- C13010CS/ywbl/gjjtq/ltxhtq/ltxhtq.js
+++ C13010CS/ywbl/gjjtq/ltxhtq/ltxhtq.js
@@ -3,23 +3,32 @@ const app = getApp();
 
 let jcrtqxxcxData;//银行信息
 let jcrtqxxcxDic;//银行
+let yhxxArray
 let gjjzhxxcxDic;
 let lixiGlobal;
 let tqjehjGlobal;
+let ywlshGlobal;
 Page({
   data: {
+   canReLaunch: !!my.reLaunch,
    tqrxm:'',
    tqrzjhm:'',
    grzhye:'',
    xhlx:'',
    tqjehj:'',
+   grzhyemc:'0.00 元',
+   xhlxmc:'0.00 元',
+   tqjehjmc:'0.00 元',
    skyhzh:'',
-   skyh:''
+   skyh:'',
+   index: 0,
+   array:[]
   },
 
   onLoad() {
     this.Serchyhxx(this);
     this.getGjjzhxxcx(this);
+    this.ywlshcx(this);
   },
 
  
@@ -43,7 +52,7 @@ Page({
         console.log("银行信息查询返回：",result);
         if (result.data.ret!="99") {
           jcrtqxxcxData=result.data;
-          console.log("jcrtqxxcxData>>>>:",jcrtqxxcxData.data.length);
+          console.log("jcrtqxxcxData>>>>:",jcrtqxxcxData.data);
           if(result.data.data.length>0){
            jcrtqxxcxDic = jcrtqxxcxData.data[0];
            that.setData({
@@ -51,9 +60,12 @@ Page({
              skyhzh: jcrtqxxcxDic.yhzh
             });
           }else {
+           jcrtqxxcxData=[];
+           //that.pickerClick(that);
            my.hideLoading();
            my.alert({ content: "未查询到您的收款银行账号信息，请添加后再来操作" });
           }
+          that.pickerClick(that);
         } else {
           my.hideLoading();
           my.alert({ content: result.data.msg });
@@ -65,7 +77,57 @@ Page({
       },
     });
   },
-
+  //选择银行卡
+  pickerClick:(that)=> {
+    yhxxArray = [];
+    if(jcrtqxxcxData.data != null && jcrtqxxcxData.data.length > 0){
+      for (let i = 0; i < jcrtqxxcxData.data.length; i++) {
+      console.log('21412412');
+      yhxxArray.push(jcrtqxxcxData.data[i].yhzh);
+      //yhxxArray.push( {'label':jcrtqxxcxData.data[i].yhzh,'value':i});
+      }
+    }
+    yhxxArray.push("添加新银行卡");
+    console.log("yhxxArray<<<<<<<:",yhxxArray);
+    that.setData({
+      array:yhxxArray,
+    });
+    // my.showActionSheet({
+    //  // title: '',
+    //   items: yhxxArray,
+    //   success: (res) => {
+    //     console.log("res",res.index);
+    //     if(res.index == jcrtqxxcxData.data.length){
+    //       let map ='ltxhtq,sssss ';
+    //       my.navigateTo({ url: '../../gjjtq/addYhxx/addYhxx?map='+map })
+    //       //my.navigateTo({ url: '../../gjjtq/addYhxx/addYhxx'});
+    //     }else{
+    //       jcrtqxxcxDic = jcrtqxxcxData.data[res.index];
+    //       this.setData({
+    //         skyh: jcrtqxxcxDic.khyh,
+    //         skyhzh: jcrtqxxcxDic.yhzh
+    //       });
+    //     }
+    //   },
+    // });
+  },
+  bindPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value);
+     if(e.detail.value!="-1"){
+      
+      if(e.detail.value == jcrtqxxcxData.data.length){
+        let map ='ltxhtq,sssss';
+        my.navigateTo({ url: '../../gjjtq/addYhxx/addYhxx?map='+map });
+      }else{
+       jcrtqxxcxDic = jcrtqxxcxData.data[e.detail.value];
+       this.setData({
+        index: e.detail.value,
+        skyh: jcrtqxxcxDic.khyh,
+        skyhzh: jcrtqxxcxDic.yhzh
+       });
+      }
+    }
+  },
 
   getGjjzhxxcx:(that)=>{
     my.httpRequest({
@@ -92,7 +154,8 @@ Page({
         that.setData({
           tqrxm: gjjzhxxcxDic.xingming,
           tqrzjhm: gjjzhxxcxDic.zjhm,
-          grzhye:parseFloat(gjjzhxxcxDic.grzhye).toFixed(2)+" 元",
+          grzhye:parseFloat(gjjzhxxcxDic.grzhye).toFixed(2),
+          grzhyemc:app.fmoney(parseFloat(gjjzhxxcxDic.grzhye).toFixed(2))+" 元",
         });
         
         that.tqlxcx(gjjzhxxcxDic.grzh,gjjzhxxcxDic.dwzh,that);
@@ -137,8 +200,10 @@ Page({
       tqjehjGlobal = parseFloat(tqjehj).toFixed(2);
       console.log("tqjehjGlobal>>>>>:",tqjehjGlobal);
       that.setData({
-         xhlx:lixiGlobal+" 元",
-         tqjehj:tqjehjGlobal+" 元",
+         xhlx:lixiGlobal,
+         tqjehj:tqjehjGlobal,
+         xhlxmc:app.fmoney(lixiGlobal)+" 元",
+         tqjehjmc:app.fmoney(tqjehjGlobal)+" 元",
       });
     },
     fail: (result) => {
@@ -146,6 +211,50 @@ Page({
       my.alert({ content: "网络错误" });
     },
    });
-  }
+  },
+  
+  ywlshcx:(that)=>{
+      my.httpRequest({
+      url: app.data.url + '/app-web/public/common/hqywlsh.service',
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "citycode":app.data.zjbzxbm.substr(0, 6)
+      },
+      data: {
+        appid: "20170517000101",
+        sign: "SYWDJSKI8UYH7D7FKIUJNE45IJHYRKJ0",
+        citybm: "C13010KF",
+      },
+      dataType: 'json',
+      contentType: 'application/json;charset=UTF-8', //contentType很重要  
+    success: (result) => {  
+      console.log("业务流水号查询返回：",result);
+      ywlshGlobal=result.data.data.ywlsh;
+    },
+    fail: (result) => {
+      my.hideLoading();
+      my.alert({ content: "网络错误" });
+    },
+   });
+  },
 
+  submitClick(){
+
+   let tqjehjcz=this.data.tqjehj;
+   let tqlxcz=this.data.xhlx;
+   let grzhyecz=this.data.grzhye;
+   let yhzhcz=this.data.skyh;
+   let khyhcz=this.data.skyhzh;
+   let tqfs="02";
+   let tqyybm="0201";
+   let ywlshcz=ywlshGlobal;
+   if(parseFloat(tqjehjcz) > 300000){
+      my.alert({ content: "提取金额大于等于30万，请到中心柜台办理业务" });
+      return;
+   }
+   let map ='ltxhtq,'+this.data.tqrxm+','+this.data.tqrzjhm+','+grzhyecz+','+tqlxcz+','+tqjehjcz+','+khyhcz+','+yhzhcz+',02,0201,'+ywlshcz;
+   console.log("map>>>>>>>>>:",map);
+   my.navigateTo({ url: '../../gjjtq/ltxhtq/ltxhtq02?map='+map });
+  }
 });
