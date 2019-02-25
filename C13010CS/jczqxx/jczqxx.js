a0170d2ac123c633c0fe0d9494e61da6f2949686
qzygit <15097855099@163.com>
2019-02-25 17:07:14 +0800

    排序修改

--- C13010CS/jczqxx/jczqxx.js
+++ C13010CS/jczqxx/jczqxx.js
@@ -16,15 +16,15 @@ Page({
     ],
     activeTab: 0,
     items: [
-      
-    ],
-    jcxxItems:[
 
     ],
-    zqxxItems:[
+    jcxxItems: [
 
     ],
-    qtxxItems:[
+    zqxxItems: [
+
+    ],
+    qtxxItems: [
 
     ],
   },
@@ -39,85 +39,103 @@ Page({
       delay: '1000',
     });
     my.getStorage({
-      key:"djxx",
-      success:(res)=>{
-        if(res.data=="zjtqcx"){
+      key: "djxx",
+      success: (res) => {
+        if (res.data == "zjtqcx") {
           this.setData({
-            activeTab:2,
+            activeTab: 2,
           });
-        }else if(res.data=="zjjccx"){
+        } else if (res.data == "zjjccx") {
           this.setData({
-            activeTab:1,
+            activeTab: 1,
           });
-        }else{
+        } else {
           this.setData({
-            activeTab:0,
+            activeTab: 0,
           });
         }
       },
     });
     my.httpRequest({
-      url: app.data.url+'/app-web/personal/public/gjjywmxcx.service',
+      url: app.data.url + '/app-web/personal/public/gjjywmxcx.service',
       method: 'POST',
       headers: {
-            "Content-Type": "application/json",
-            "citycode":app.data.zjbzxbm.substr(0,6),
+        "Content-Type": "application/json",
+        "citycode": app.data.zjbzxbm.substr(0, 6),
       },
       data: {
         appid: "20170517000101",
-        zjbzxbm:app.data.zjbzxbm,
-        sign:"SYWDJSKI8UYH7D7FKIUJNE45IJHYRKJ0",
-        grzh:app.data.grzh,
-        ksrq:app.getTwoYearAgoFormatDate(),
-        jsrq:app.getNowFormatDate()
+        zjbzxbm: app.data.zjbzxbm,
+        sign: "SYWDJSKI8UYH7D7FKIUJNE45IJHYRKJ0",
+        grzh: app.data.grzh,
+        ksrq: app.getTwoYearAgoFormatDate(),
+        jsrq: app.getNowFormatDate()
       },
       dataType: 'json',
-      contentType : 'application/json;charset=UTF-8', //contentType很重要    
+      contentType: 'application/json;charset=UTF-8', //contentType很重要    
       success: (res) => {
-         let jczqxx = res.data.data;
-         let alljczqxx = [];
-         let jcxx = [];
-         let zqxx = [];
-         let qtxx = [];
-         let a=0; let b=0;let c=0;
-          let s;
-         for(let i=0; i<jczqxx.length; i++){
-           s = {};
-           s.ywfsrq = jczqxx[i].ywfsrq;
-           s.ywzy = jczqxx[i].ywzy;
-           s.yue = app.fmoney(jczqxx[i].yue);
-           s.fse = app.fmoney(jczqxx[i].fse);
-           alljczqxx[i] = s;
-            if(jczqxx[i].ywlx=="缴存"){
-              jcxx[a] = s;
-              a++;
-              continue;
-            }else if(jczqxx[i].ywlx=="提取"){
-              zqxx[b] = s;
-              b++;
-              continue;
-            }else if(jczqxx[i].ywlx=="其他"){
-              qtxx[c] = s;
-              c++;
-              continue;
-            }
-         }
-        a=0;
-        b=0;
-        c=0;
+        let jczqxx = res.data.data;
+        let alljczqxx = [];
+        let jcxx = [];
+        let zqxx = [];
+        let qtxx = [];
+        let a = 0; let b = 0; let c = 0; let e = 0;
+        let flagtq; let flagqt;
+        let s;
+        for (let i = jczqxx.length - 1; i > 0; i--) {
+          s = {};
+          s.ywfsrq = jczqxx[i].ywfsrq;
+          s.ywzy = jczqxx[i].ywzy;
+          s.yue = app.fmoney(jczqxx[i].yue);
+          s.fse = app.fmoney(jczqxx[i].fse);
+          alljczqxx[e] = s;
+          e++;
+          if (jczqxx[i].ywlx == "缴存") {
+            jcxx[a] = s;
+            a++;
+            continue;
+          } else if (jczqxx[i].ywlx == "提取") {
+            zqxx[b] = s;
+            b++;
+            continue;
+          } else if (jczqxx[i].ywlx == "其他") {
+            qtxx[c] = s;
+            c++;
+            continue;
+          }
+        }
+        a = 0;
+        b = 0;
+        c = 0;
+        e = 0;
+
+        if (zqxx.length < 1) {
+          flagtq = 0;
+        } else {
+          flagtq = 1;
+        }
+        if (qtxx.length < 1) {
+          flagqt = 0;
+        } else {
+          flagqt = 1;
+        }
+        console.log("zqxx", zqxx, zqxx.length, "flagtq", flagtq);
+        console.log("qtxx", qtxx, qtxx.length, "flagqt", flagqt);
         this.setData({
-          items:alljczqxx,
-          jcxxItems:jcxx,
-          zqxxItems:zqxx,
-          qtxxItems:qtxx
+          items: alljczqxx,
+          jcxxItems: jcxx,
+          zqxxItems: zqxx,
+          qtxxItems: qtxx,
+          flagqt: flagqt,
+          flagtq: flagtq,
         });
         my.hideLoading();
       },
-      fail:(res) => {
+      fail: (res) => {
         my.hideLoading();
-        my.alert({content:"网络错误"});
+        my.alert({ content: "网络错误" });
       },
-      complete:(res) => {
+      complete: (res) => {
         my.hideLoading();
         // my.alert({title: 'complete'});
       }
